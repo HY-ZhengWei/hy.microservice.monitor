2c04dde892020c26b424ae567b784480d3a8218c
HY-ZhengWei <HY.ZhengWei@qq.com>
2021-05-08 15:40:36 +0800

    1.0.2
    1。添加：发微信预警

--- hy.microservice.monitor/src/main/java/org/hy/microservice/monitor/message/MessageService.java
+++ hy.microservice.monitor/src/main/java/org/hy/microservice/monitor/message/MessageService.java
@@ -31,6 +31,9 @@ public class MessageService
     @Xjava(ref="XHTTP_MS_Monitor_Message_SendSMS")
     private XHttp xhSendSMS;
     
+    @Xjava(ref="XHTTP_MS_Monitor_Message_SendWeiXin")
+    private XHttp xhSendWeiXin;
+    
     @Xjava
     private XSSOUserService ssoUserService;
     
@@ -69,12 +72,77 @@ public class MessageService
             Map<String ,Object> v_ReqParams = new HashMap<String ,Object>();
             v_ReqParams.put("token" ,v_Token);
             
-            SMSInfo v_SMSInfo = new SMSInfo();
+            Reciver v_Reciver = new Reciver();
             
-            v_SMSInfo.setPhone(i_Phone);
-            v_SMSInfo.setMessage(i_Content);
+            v_Reciver.setPhone(i_Phone);
+            v_Reciver.setMessage(i_Content);
             
-            Return<?> v_Ret = xhSendSMS.request(v_ReqParams ,v_XJson.toJson(v_SMSInfo).toJSONString());
+            Return<?> v_Ret = xhSendSMS.request(v_ReqParams ,v_XJson.toJson(v_Reciver).toJSONString());
+            
+            if ( v_Ret != null && v_Ret.booleanValue() && !Help.isNull(v_Ret.getParamStr()) )
+            {
+                BaseResp v_Data = (BaseResp)v_XJson.toJava(v_Ret.getParamStr() ,BaseResp.class);
+                
+                if ( v_Data != null )
+                {
+                    if ( $Succeed.equals(v_Data.getCode()) )
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        catch (Exception exce)
+        {
+            exce.printStackTrace();
+        }
+        
+        return false;
+    }
+    
+    
+    
+    /**
+     * 发送微信
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2021-02-08
+     * @version     v1.0
+     *
+     * @param i_OpenID    微信的OpenID
+     * @param i_Title     标题
+     * @param i_Content   消息内容
+     * @return
+     */
+    public boolean weixin(String i_Phone ,String i_Title ,String i_Content)
+    {
+        UserSSO v_UserSSO = new UserSSO();
+        
+        v_UserSSO.setUserId("hy.micorservice.monitor");
+        v_UserSSO.setUserName("监控服务");
+        v_UserSSO.setUserSource("监控服务");
+        
+        String v_Token = ssoUserService.loginUser(v_UserSSO);
+        
+        if ( Help.isNull(v_UserSSO) )
+        {
+            return false;
+        }
+        
+        try
+        {
+            XJSON v_XJson = new XJSON();
+            
+            Map<String ,Object> v_ReqParams = new HashMap<String ,Object>();
+            v_ReqParams.put("token" ,v_Token);
+            
+            Reciver v_Reciver = new Reciver();
+            
+            v_Reciver.setPhone(i_Phone);
+            v_Reciver.setTitle(i_Title);
+            v_Reciver.setMessage(i_Content);
+            
+            Return<?> v_Ret = xhSendSMS.request(v_ReqParams ,v_XJson.toJson(v_Reciver).toJSONString());
             
             if ( v_Ret != null && v_Ret.booleanValue() && !Help.isNull(v_Ret.getParamStr()) )
             {
